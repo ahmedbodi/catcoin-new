5a810e6bf9d88384b7e42b3a822256fc0fac7d5e
Carlos Pizarro <kr105@kr105.com>
2024-09-23 16:37:30 -0300

    Fix compilation with recent version of miniupnpc

--- src/net.cpp
+++ src/net.cpp
@@ -1636,8 +1636,11 @@ static void ThreadMapPort()
     struct UPNPUrls urls;
     struct IGDdatas data;
     int r;
-
+#if (MINIUPNPC_API_VERSION >= 18)
+    r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr), nullptr, 0);
+#else
     r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
+#endif
     if (r == 1)
     {
         if (fDiscover) {
